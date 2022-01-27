22431f840f75f1b8022237ebde245c090e85c936
pablohpaiva22 <pablohpaiva22@gmail.com>
2022-01-27 16:41:20 -0300

    criando script accordionList

--- js/scrips.js
+++ js/scrips.js
@@ -24,3 +24,23 @@ parametros.forEach(ativarParametros)
 
 // -------------------------------------------------------
 
+const faqAsk = document.querySelectorAll('.faq dt')
+const faqAnswer = document.querySelectorAll('.faq dd')
+const button = document.querySelectorAll('.faq button')
+
+function accordionList(item, index) {
+    item.addEventListener('click', function() { 
+        faqAnswer[index].classList.toggle('ativo')
+        const id = button[index].getAttribute('aria-expanded')
+        if(id == 'true') {
+            button[index].setAttribute('aria-expanded', 'false')
+        }
+        if(id == 'false') {
+            button[index].setAttribute('aria-expanded', 'true')
+        }
+    })
+}
+
+faqAsk.forEach(accordionList)
+
+// -------------------------------------------------------
